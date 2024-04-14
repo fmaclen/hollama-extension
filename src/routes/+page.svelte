2f88a0238694600d60b46c62faa0e0139c3ab1c6
Fernando Maclen <fmaclen@gmail.com>
2024-04-13 22:47:58 -0400

    extract innerHTML from current tab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let extractedText = '';
+
+	async function extractText() {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if (tab.id) {
+      const html = await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+          return document.body.innerHTML;
+        },
+      });
+
+      extractedText = html[0].result || '';
+    }
+	}
+
+	function copyText() {
+		navigator.clipboard.writeText(extractedText);
+	}
+
+	function saveAsFile() {
+		const blob = new Blob([extractedText], { type: 'text/plain' });
+		const url = URL.createObjectURL(blob);
+		const a = document.createElement('a');
+		a.href = url;
+		a.download = 'extracted-text.txt';
+		a.click();
+		URL.revokeObjectURL(url);
+	}
+</script>
+
+<h1>Hollama</h1>
+
+<hr />
+
+<button on:click={extractText}>Extract</button>
+<button on:click={copyText}>Copy</button>
+<button on:click={saveAsFile}>Save as file</button>
+
+<hr />
+
+<pre>
+  <code>
+    {extractedText}
+  </code>
+</pre>
+
+<style>
+	pre,
+	code {
+		width: 100%;
+		overflow: hidden;
+		max-height: 320px;
+	}
+</style>
